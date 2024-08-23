168225b3dc6532cb41a7b598739bc34a12968d84
Cerberus91DE <122625107+Cerberus91DE@users.noreply.github.com>
2024-08-23 11:43:01 +0200

    Update bannhammer.user.js

--- bannhammer.user.js
+++ bannhammer.user.js
@@ -2,7 +2,7 @@
 // @name            Bannliste - cerberus91_de Edition
 // @description     A tool for moderating Twitch easier during hate raids
 // @namespace       Bannliste - cerberus91_de Edition
-// @version         1.0.0.1
+// @version         1.0.0.0
 // @match           *://www.twitch.tv/*
 // @run-at          document-idle
 // @author          TwitchModsDACH - The original code is from victornpb
